6be3a2785e1aaa945962f38600158dfb4a249b7e
Philip Crockett <philipmayoo@gmail.com>
2017-01-19 12:36:00 +0100

    Added description

--- chapter2/excer2-1.c
+++ chapter2/excer2-1.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
 #include <limits.h>
 #include <float.h>
+
+/*
+Write a program to determine the ranges of char, short, int and long variables, both signed and unsigned, by printing appropriate values from standard headers and direct computation. Harder if you compute them: determine the ranges of the various floating-point types.
+*/
 
 int main() 
 {
