692845c1ccab8e893291fbd74eeaa621b66ccd05
Philip Crockett <philipmayoo@gmail.com>
2017-02-04 10:37:35 +0000

    Working on 5_7, not completed yet

---
+++ chapter5/exercise5_7.c
@@ -0,0 +1,111 @@
+// Sorting program from the C-programming language by K&R
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+#define MAXLEN 1000
+
+char *lineptr[MAXLINES];
+static char allocbuf[MAXLEN];
+char *allocp = allocbuf;
+
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+int getline_c(char s[], int lim);
+char *alloc(int);
+
+void qsort(char *lineptr[], int left, int right);
+
+int main()
+{
+	int nlines;
+
+	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+		qsort(lineptr, 0, nlines - 1);
+		writelines(lineptr, nlines);
+		return 0;
+	} else {
+		printf("error: input to big to sort\n");
+		return 1;
+	}
+}
+
+
+
+int readlines(char *lineptr[], int maxlines)
+{
+	int len, nlines;
+	char *p, line[MAXLEN];
+	
+	nlines = 0;
+	while ((len = getline_c(line, MAXLEN) > 0)) {
+			if (nlines >= maxlines || (p = alloc(len)) == NULL) {
+				return -1;
+			} else {
+				line[len-1] = '\0';
+				strcpy(p, line);
+				lineptr[nlines++] = p;
+			}
+		}
+	return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+	while (nlines-- > 0)
+		printf("%s\n", *lineptr++);
+}
+		
+int getline_c(char s[], int lim)
+{
+	int i, c;
+
+	for (i = 0; i < lim - 1 && (c=getchar()) != EOF && c!='\n'; ++i, *s++)
+		s[i] = c;
+
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+
+char *alloc(int n)
+{
+	if (allocbuf + MAXLEN - allocp >= n) {
+		allocp += n;
+		return allocp - n;
+	} else {
+		return 0;
+	}
+}
+
+void qsort(char *v[], int left, int right)
+{
+	int i, last;
+	void swap(char *v[], int i, int j);
+
+	if (left >= right)
+		return;
+	swap(v, left, (left + right)/2);
+	last = left;
+	for(i = left+1; i <= right; i++)
+		if (strcmp(v[i], v[left]) < 0)
+			swap(v, ++last, i);
+	swap(v, left, last);
+	qsort(v, left, last-1);
+	qsort(v, last+1, right);
+}
+
+void swap(char *v[], int i, int j)
+{
+	char *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
